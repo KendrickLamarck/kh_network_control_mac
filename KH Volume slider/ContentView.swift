fcb20ffd2c794ba1f9232c841bb2748b5cae128e
Leander Blume <leander@blume.ag>
2024-12-21 20:32:22 +0100

    main file

--- KH Volume slider/ContentView.swift
+++ KH Volume slider/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var volume: Double = 55
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Slider(value: $volume, in: 0...120, step: 1) {
+                Text("")
+            } minimumValueLabel: {
+                Text("0")
+            } maximumValueLabel: {
+                Text("120")
+            }
+            Text("Volume: \(Int(volume))")
         }
         .padding()
+        .onChange(of: volume) {
+            // Call Python script
+            let process = Process()
+            let pipe = Pipe()
+            process.standardOutput = pipe
+            process.executableURL = URL(filePath: "/bin/sh")
+            process.currentDirectoryURL = URL(filePath: "/Users/lblume/code/kh_120/")
+            process.arguments = ["-c", "/opt/homebrew/bin/python3.9", "/Users/lblume/code/kh_120/khtool/khtool.py",
+                                 "-i", "en0",
+                                 "--level", "\(Int(volume))"]
+            try! process.run()
+        }
+        .frame(width: 300, height: 100)
+
     }
 }
 
