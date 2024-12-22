55dfcdca6563c27c6363a17b9df1979325650ec1
Leander Blume <leander@blume.ag>
2024-12-22 12:32:01 +0100

    Small improvements

--- KH Volume slider/ContentView.swift
+++ KH Volume slider/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var volume: Double = 55
+    // This should really read out the actual current volume...
+    @State private var volume: Double = 54
 
     var body: some View {
         VStack {
             Text("Monitor volume")
-            Slider(value: $volume, in: 0...120, step: 5) {
+            Slider(value: $volume, in: 0...120, step: 6) {
                 Text("")
             } minimumValueLabel: {
                 Text("0")
@@ -25,6 +26,7 @@ struct ContentView: View {
                 }
                 // Call Python script
                 let process = Process()
+                // We can't call python directly for some reason. Also needed to disable the app sandbox in the entitlement file or something to make this work.
                 process.executableURL = URL(filePath: "/bin/sh")
                 process.currentDirectoryURL = URL(filePath: "/Users/lblume/code/kh_120/")
                 process.arguments =
