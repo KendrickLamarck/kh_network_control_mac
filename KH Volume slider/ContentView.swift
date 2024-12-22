885d42161961930fb816599f6305eb5c13b97355
Leander Blume <leander@blume.ag>
2024-12-22 12:09:47 +0100

    Much better slider responsivity
    Changes now only apply once the slider stops moving.

--- KH Volume slider/ContentView.swift
+++ KH Volume slider/ContentView.swift
@@ -12,32 +12,31 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Text("KH 120 II volume")
+            Text("Monitor volume")
             Slider(value: $volume, in: 0...120, step: 5) {
                 Text("")
             } minimumValueLabel: {
                 Text("0")
             } maximumValueLabel: {
                 Text("120")
+            } onEditingChanged: { editing in
+                if (editing) {
+                    return
+                }
+                // Call Python script
+                let process = Process()
+                process.executableURL = URL(filePath: "/bin/sh")
+                process.currentDirectoryURL = URL(filePath: "/Users/lblume/code/kh_120/")
+                process.arguments =
+                    ["-c",
+                     "/opt/homebrew/bin/python3.9"
+                         + " /Users/lblume/code/kh_120/khtool/khtool.py"
+                         + " -i en0 --level \(Int(volume))"]
+                try! process.run()
             }
             Text("\(Int(volume)) dB")
         }
         .padding()
-        .onChange(of: volume) {
-            // Call Python script
-            let process = Process()
-            let pipe = Pipe()
-            process.standardOutput = pipe
-            process.executableURL = URL(filePath: "/bin/sh")
-            process.currentDirectoryURL = URL(filePath: "/Users/lblume/code/kh_120/")
-            process.arguments =
-                ["-c",
-                 "/opt/homebrew/bin/python3.9"
-                     + " /Users/lblume/code/kh_120/khtool/khtool.py"
-                     + " -i en0 --level \(Int(volume))"]
-            try! process.run()
-            Thread.sleep(forTimeInterval: 0.1) // this sucks but it works
-        }
         .frame(width: 300, height: 80)
     }
 }
